88c225651920d5bb1f30a8d32c584caafe11a960
ZekerDev <73775774+ZekerDev@users.noreply.github.com>
2020-12-14 13:22:35 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1607962307,
+	"fileList": [
+		"data.js",
+		"hrruntime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/snake_head-sheet0.png",
+		"images/snake_body-sheet0.png",
+		"images/grass_hopper-sheet0.png",
+		"images/grass-sheet0.png",
+		"images/spritefont.png",
+		"media/squaremotif1.m4a",
+		"media/squaremotif1.ogg",
+		"media/oops3.m4a",
+		"media/oops3.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
